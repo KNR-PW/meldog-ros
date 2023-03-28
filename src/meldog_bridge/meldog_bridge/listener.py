e2105118efe9a13129a65e2f2cee3cd59690a0eb
jmacuga <julia.macuga@gmail.com>
2023-03-28 21:22:31 +0200

    minimal talker and listener implemented

---
+++ src/meldog_bridge/meldog_bridge/listener.py
@@ -0,0 +1,33 @@
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import String
+
+
+class Listener(Node):
+
+    def __init__(self):
+        super().__init__('listener')
+        self.subscription = self.create_subscription(
+            String,
+            'topic',
+            self.listener_callback,
+            10)
+        self.subscription  # prevent unused variable warning
+
+    def listener_callback(self, msg):
+        self.get_logger().info('I heard: "%s"' % msg.data)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    listener = Listener()
+
+    rclpy.spin(listener)
+    listener.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
