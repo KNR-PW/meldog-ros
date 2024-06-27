8f8b70732f20d3eb13a12712188f0a389bf67a28
Bartek <bartlomiejk@vp.pl>
2024-06-27 14:39:16 +0200

    Poprawiono liniową trajektorię i dodano do niej launch file'a

---
+++ src/meldog_tests/meldog_leg_tests/meldog_leg_tests/launch/linear_trajectory_demo.launch.py
@@ -0,0 +1,37 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+
+LENGTH_1 = 0.225
+LENGTH_2 = 0.225
+GEAR_RATIO = 16
+START_POSITION = [0.0, -0.30]
+RADIUS = 0.1
+ANGULAR_VALOCITY = 3.14
+def generate_launch_description():
+    inverse_kinematics = Node(
+        package = 'meldog_leg_tests',
+        executable = 'inverse_kinematics_2D',
+        parameters = [{
+            'length_1': LENGTH_1,
+            'length_2': LENGTH_2,
+            'gear_ratio': GEAR_RATIO,
+            'start_position': START_POSITION
+        }],
+        output = 'screen'
+    )
+    linear_trajectory = Node(
+        package = 'meldog_leg_tests',
+        executable = 'linear_trajectory',
+        parameters = [{
+            'start_position': START_POSITION,
+            'radius': RADIUS,
+            'angular_velocity': ANGULAR_VALOCITY
+        }],
+        output = 'screen'
+    )
+
+    nodes = [inverse_kinematics, linear_trajectory]
+    ld = LaunchDescription()
+    ld.add_action()
