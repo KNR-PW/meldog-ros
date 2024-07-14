52ec0304377de1330d1b39e9f40d026da561223a
Bartek <bartlomiejk@vp.pl>
2024-07-14 18:06:27 +0200

    Modified pi3hat hardware interface

--- src/meldog_hardware/pi3hat_hardware_interface/include/pi3hat_hardware_interface/pi3hat_hardware_interface.hpp
+++ src/meldog_hardware/pi3hat_hardware_interface/include/pi3hat_hardware_interface/pi3hat_hardware_interface.hpp
@@ -5,6 +5,7 @@
 #include <memory>
 #include <string>
 #include <vector>
+#include <map>
 
 #include "hardware_interface/handle.hpp"
 #include "hardware_interface/hardware_info.hpp"
@@ -17,10 +18,10 @@
 #include "rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp"
 #include "rclcpp_lifecycle/state.hpp"
 
-#include "actuator_wrappers/MoteusWrapper.hpp"
-
-#include "pi3hat/pi3hat.h"
-#include "pi3hat/realtime.h"
+#include "../actuator_wrappers/MoteusWrapper.hpp"
+#include "../imu/IMUTransform.hpp"
+#include "../pi3hat/pi3hat.h"
+#include "../pi3hat/realtime.h"
 
 #include "visibility_control.hpp"
 
@@ -64,75 +65,82 @@ namespace pi3hat_hardware_interface
             const rclcpp::Time &time, const rclcpp::Duration &period) override;
 
     private:
+        /* PART FOR COMMUNICATION WITH HARDWARE: */
 
+        /* number of actuators */
         size_t number_of_actuators;
 
+        /* Pi3hat */
         std::shared_ptr<mjbots::pi3hat::Pi3Hat> pi3hat_;
-        mjbots::pi3hat::Pi3Hat::Input pi3hat_input_;
-        mjbots::pi3hat::Attitude attitude_;
 
-        std::shared_ptr<mjbots::pi3hat::CanFrame[]> tx_can_frames_; //Pomysl jeszcze nad tym!
-        std::shared_ptr<mjbots::pi3hat::CanFrame[]> rx_can_frames_;
+        /* Pi3hat input structure */
+        mjbots::pi3hat::Pi3Hat::Input pi3hat_input_;
 
         /* IMU states */ 
-        std::array<double, 4> hw_state_imu_orientation_;         // x, y, z, w
-        std::array<double, 3> hw_state_imu_angular_velocity_;    // x, y, z
-        std::array<double, 3> hw_state_imu_linear_acceleration_; // x, y, z
+        mjbots::pi3hat::Attitude attitude_;
 
-        /* Actuator CAN config */
-        std::vector<int> hw_actuator_can_buses_;  
-        std::vector<int> hw_actuator_can_ids_;
+        /* IMU transform */
+        IMU::IMUTransform imu_transform_;
 
-        /* Actuator parameters */
-        std::vector<double> hw_actuator_position_offsets_; // To wrzucimy do opcji
+        /* TX CAN frames */
+        std::shared_ptr<mjbots::pi3hat::CanFrame[]> tx_can_frames_;
 
-        /* Actuator limits */
-        std::vector<double> hw_actuator_position_mins_; 
-        std::vector<double> hw_actuator_position_maxs_; // To wrzucimy do opcji
-        std::vector<double> hw_actuator_velocity_maxs_;
-        std::vector<double> hw_actuator_effort_maxs_;
+        /* RX CAN frames */ 
+        std::shared_ptr<mjbots::pi3hat::CanFrame[]> rx_can_frames_;
 
-        /* Actuator states */
+        /* Container for motor_id -> joint_index maping */
+        std::map<int, int> actuator_joint_map_;
+
+        /* Actuator states and commands */
         std::vector<actuator_wrappers::ActuatorState> hw_actuator_states_;
-
-        //  Actuator commands
         std::vector<actuator_wrappers::ActuatorCommand> hw_actuator_commands_;
 
-        /* Actuator Wrappers (here change to your own wrapper) */
+        /* Actuator Wrappers (HERE change to your own wrapper) */
         std::vector<actuator_wrappers::MoteusWrapper> moteus_wrappers;
+
+
+        using JointState = actuator_wrappers::ActuatorState;
+        using JointCommand = actuator_wrappers::ActuatorCommand;
+
+        /* Joint states and commands (for transmissions)*/
+        std::vector<JointState> hw_joint_states_;
+        std::vector<JointCommand> hw_joint_commands_;
 
         /* Function for creating moteus wrappers (here u can add your own wrapper)
            Remember to change this function in source code */
         void add_actuator_wrapper(const hardware_interface::HardwareInfo &info, const WrapperType type);
-        
+
         template<class Wrapper>
         void commands_to_tx_frames(std::vector<actuator_wrappers::ActuatorWrapperBase<Wrapper>> actuator_wrappers)
         {
             // TODO: Uporządkuj wcześniej silniki względem id
-            for(size_t i = 0; i < number_of_actuators; i++)
+            for(int i = 0; i < number_of_actuators; i++)
             {   
-                size_t actuator_id = tx_can_frames_[i].id;
-                actuator_wrappers[actuator_id].command_to_tx_frame(tx_can_frames_[i], hw_actuator_commands_[actuator_id]);
+                actuator_wrappers[i].command_to_tx_frame(tx_can_frames_[i], hw_actuator_commands_[i]);
             }
         };
 
         template<class Wrapper>
         void rx_frames_to_states(std::vector<actuator_wrappers::ActuatorWrapperBase<Wrapper>> actuator_wrappers)
         {
-            for(size_t i = 0; i < number_of_actuators; i++)
+            for(int i = 0; i < number_of_actuators; i++)
             {   
-                size_t actuator_id = tx_can_frames_[i].id;
-                actuator_wrappers[actuator_id].rx_frame_to_state(rx_can_frames_[i], hw_actuator_states_[actuator_id]);
+                int joint_index = actuator_joint_map_[rx_can_frames_[i].id];
+                actuator_wrappers[joint_index].rx_frame_to_state(rx_can_frames_[i], hw_actuator_states_[joint_index]);
             }
         }
+
+        /* PART FOR CREATING TRANSMISSION OBJECTS:*/
+        
     };
 
-/* Here add your actuator wrapper type */
-enum WrapperType
-{
-    Moteus = 0,
 
-};
+    /* Here add your actuator wrapper type */
+    enum WrapperType
+    {
+        Moteus = 0,
+
+    };
 
 }; 
 
