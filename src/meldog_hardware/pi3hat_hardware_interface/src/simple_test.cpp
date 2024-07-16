63ce53fc00e236977f008e6f9243c14a045acc3d
Bartek <bartlomiejk@vp.pl>
2024-07-16 19:08:05 +0200

    Added simple example

---
+++ src/meldog_hardware/pi3hat_hardware_interface/src/simple_test.cpp
@@ -0,0 +1,80 @@
+#include "../include/pi3hat/pi3hat.h"
+#include "../include/pi3hat/realtime.h"
+#include "../include/moteus/moteus.h"
+
+
+static double GetNow() 
+{
+  struct timespec ts = {};
+  ::clock_gettime(CLOCK_MONOTONIC_RAW, &ts);
+  return static_cast<double>(ts.tv_sec) +
+      static_cast<double>(ts.tv_nsec) / 1e9;
+};
+
+int main(int argc, char** argv)
+{
+    // moteus part
+    using mjbots::moteus::Controller;
+    Controller::Options moteus_options;
+    moteus_options.bus = 1;
+    moteus_options.id = 1;
+    Controller moteus_controller(moteus_options);
+
+    mjbots::moteus::PositionMode::Command moteus_command;
+
+    //pi3hat part
+
+    mjbots::pi3hat::Pi3Hat::Configuration pi3hat_configuration;
+    pi3hat_configuration.attitude_rate_hz = 1000;
+    pi3hat_configuration.can[0].automatic_retransmission = false;
+    pi3hat_configuration.can[0].fdcan_frame = false;
+
+    mjbots::pi3hat::CanFrame tx_frame;
+    mjbots::pi3hat::CanFrame rx_frame;
+    tx_frame.id = 1; 
+    tx_frame.bus = 1;
+    mjbots::pi3hat::Span<mjbots::pi3hat::CanFrame> tx_span(&tx_frame, 1);
+    mjbots::pi3hat::Span<mjbots::pi3hat::CanFrame> rx_span(&rx_frame, 1);
+    mjbots::pi3hat::Attitude attitude;
+
+    mjbots::pi3hat::Pi3Hat::Input input;
+    mjbots::pi3hat::Pi3Hat::Output pi3hat_output;
+    input.tx_can = tx_span;
+    input.rx_can = rx_span;
+    input.attitude = &attitude;
+    input.request_attitude = true;
+
+    mjbots::pi3hat::Pi3Hat pi3hat(pi3hat_configuration);
+
+    // Buffer for printing info
+    char buf[4096] = {};
+
+    auto prev = GetNow();
+    double frequency;
+    std::string status_line;
+    while(true)
+    {
+        auto now = GetNow();
+        double position_ = 5 * sin(now - prev);
+        moteus_command.position = position_;
+        mjbots::moteus::CanFdFrame can_fd_frame = moteus_controller.MakePosition(moteus_command);
+
+        /* Copy data from CANFD frame to CAN frame*/
+        tx_frame.size = can_fd_frame.size;
+        std::memcpy(tx_frame.data, can_fd_frame.data, can_fd_frame.size);
+
+
+        auto mesaure_time = GetNow() - now;
+        frequency = 1/mesaure_time;
+        pi3hat_output = pi3hat.Cycle(input);
+        mjbots::moteus::Query::Result result = mjbots::moteus::Query::Parse(rx_frame.data, rx_frame.size);
+        double state_position = result.position;
+        ::snprintf(buf, sizeof(buf) -1, "f/p/v/t=(%7.3f, %7.3f) ",
+        frequency, state_position);
+        status_line += buf;
+        ::printf("%s\n  \r", status_line.c_str());
+        ::fflush(::stdout);
+
+    };
+    return 0;
+}
