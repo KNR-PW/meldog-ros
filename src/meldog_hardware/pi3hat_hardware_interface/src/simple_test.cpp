5698465200d088d516f751f4928aaea6c07c7ab8
Bartek <bartlomiejk@vp.pl>
2024-07-19 14:54:16 +0200

    Testing

--- src/meldog_hardware/pi3hat_hardware_interface/src/simple_test.cpp
+++ src/meldog_hardware/pi3hat_hardware_interface/src/simple_test.cpp
@@ -17,7 +17,7 @@ int main(int argc, char** argv)
     using mjbots::moteus::Controller;
     Controller::Options moteus_options;
     moteus_options.bus = 1;
-    moteus_options.id = 2;
+    moteus_options.id = 1;
     Controller moteus_controller(moteus_options);
 
     mjbots::moteus::PositionMode::Command moteus_command;
@@ -28,12 +28,12 @@ int main(int argc, char** argv)
     pi3hat_configuration.attitude_rate_hz = 1000;
 
     mjbots::pi3hat::CanFrame tx_frame;
-    mjbots::pi3hat::CanFrame rx_frame;
+    mjbots::pi3hat::CanFrame rx_frame[10];
     tx_frame.id = 1; 
-    tx_frame.bus = 2;
+    tx_frame.bus = 1;
     tx_frame.expect_reply = true;
     mjbots::pi3hat::Span<mjbots::pi3hat::CanFrame> tx_span(&tx_frame, 1);
-    mjbots::pi3hat::Span<mjbots::pi3hat::CanFrame> rx_span(&rx_frame, 1);
+    mjbots::pi3hat::Span<mjbots::pi3hat::CanFrame> rx_span(rx_frame, 10);
     mjbots::pi3hat::Attitude attitude;
 
     mjbots::pi3hat::Pi3Hat::Input input;
@@ -45,6 +45,11 @@ int main(int argc, char** argv)
 
     mjbots::pi3hat::Pi3Hat pi3hat(pi3hat_configuration);
 
+    mjbots::moteus::CanFdFrame can_fd_frame = moteus_controller.MakeStop();
+    tx_frame.size = can_fd_frame.size;
+    std::memcpy(tx_frame.data, can_fd_frame.data, can_fd_frame.size);
+    pi3hat_output = pi3hat.Cycle(input);
+    std::cout << "Started: " << pi3hat_output.rx_can_size << std::endl;
     // Buffer for printing info
     char buf[4096] = {};
 
@@ -66,7 +71,7 @@ int main(int argc, char** argv)
         auto mesaure_time = GetNow() - now;
         frequency = 1/mesaure_time;
         pi3hat_output = pi3hat.Cycle(input);
-        mjbots::moteus::Query::Result result = mjbots::moteus::Query::Parse(rx_frame.data, rx_frame.size);
+        mjbots::moteus::Query::Result result = mjbots::moteus::Query::Parse(rx_frame[0].data, rx_frame[0].size);
         double state_position = result.position;
         std::cout << "Attitude?: " << pi3hat_output.attitude_present << std::endl;
         std::cout << "Data size?: " << pi3hat_output.rx_can_size << std::endl;
