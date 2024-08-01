1a619ec9eef808469bc016ccb9e39ce31ff163e9
Bartek <bartlomiejk@vp.pl>
2024-08-01 22:59:43 +0200

    Changed constructor for MoteusWrapper class

--- src/meldog_hardware/pi3hat_hardware_interface/include/controllers/wrappers/MoteusWrapper.hpp
+++ src/meldog_hardware/pi3hat_hardware_interface/include/controllers/wrappers/MoteusWrapper.hpp
@@ -3,6 +3,7 @@
 
 #include "ControllerWrapper.hpp"
 #include "../../3rd_libs/moteus/moteus.h"
+#include <memory>
 
 namespace controller_interface
 {
@@ -18,14 +19,12 @@ class MoteusWrapper final: public ControllerWrapper
 
     /* Command structure for moteus object*/
     mjbots::moteus::PositionMode::Command position_command_;
-    mjbots::moteus::Controller moteus_controller_;
+    std::unique_ptr<mjbots::moteus::Controller> moteus_controller_;
     
     public:
     using CanFrame = mjbots::pi3hat::CanFrame;
 
-    MoteusWrapper( 
-        const mjbots::moteus::Controller::Options& options,
-        const mjbots::moteus::PositionMode::Command& command);
+    MoteusWrapper(const ControllerParameters params);
     void command_to_tx_frame(CanFrame& tx_frame, const ControllerCommand& command) override;
     void rx_frame_to_state(const CanFrame& rx_frame, ControllerState& state) override;
     void init_to_tx_frame(CanFrame& tx_frame) override;
--- src/meldog_hardware/pi3hat_hardware_interface/src/controllers/wrappers/MoteusWrapper.cpp
+++ src/meldog_hardware/pi3hat_hardware_interface/src/controllers/wrappers/MoteusWrapper.cpp
@@ -2,13 +2,29 @@
 
 using namespace controller_interface;
 
-MoteusWrapper::MoteusWrapper(
-        const mjbots::moteus::Controller::Options& options,
-        const mjbots::moteus::PositionMode::Command& command):
+MoteusWrapper::MoteusWrapper(const ControllerParameters params):
+    ControllerWrapper()
+{
+    /* moteus options */ 
+    using mjbots::moteus::Controller;
+    using controller_interface::MoteusWrapper;
+    Controller::Options moteus_options;
+    moteus_options.bus = params.bus_;
+    moteus_options.id = params.id_;
 
-        ControllerWrapper(), 
-        position_command_(command),
-        moteus_controller_(mjbots::moteus::Controller(options)) {} 
+    /* moteus command format (it will be copied to wrapper) */
+    mjbots::moteus::PositionMode::Format format;
+    format.feedforward_torque = mjbots::moteus::kFloat;
+    format.maximum_torque = mjbots::moteus::kFloat;
+    format.velocity_limit= mjbots::moteus::kFloat;
+    moteus_options.position_format = format;
+
+    /* moteus command (it will be copied to wrapper) */
+    position_command_.maximum_torque = params.torque_max_;
+    position_command_.velocity_limit = params.velocity_max_;
+
+    moteus_controller_ = std::make_unique<mjbots::moteus::Controller>(moteus_options);
+} 
 
 
 void MoteusWrapper::command_to_tx_frame(CanFrame& tx_frame, const ControllerCommand& command) 
@@ -19,7 +35,7 @@ void MoteusWrapper::command_to_tx_frame(CanFrame& tx_frame, const ControllerComm
     position_command_.feedforward_torque = command.torque_;
 
     /* Create CANFD frame*/
-    mjbots::moteus::CanFdFrame can_fd_frame = moteus_controller_.MakePosition(position_command_);
+    mjbots::moteus::CanFdFrame can_fd_frame = moteus_controller_->MakePosition(position_command_);
     
     /* Copy data from CANFD frame to CAN frame */
     tx_frame.id = can_fd_frame.arbitration_id;
@@ -31,7 +47,7 @@ void MoteusWrapper::command_to_tx_frame(CanFrame& tx_frame, const ControllerComm
 void MoteusWrapper::rx_frame_to_state(const CanFrame& rx_frame, ControllerState& state) 
 {
     /* Parse data from RX CAN frame to Result object */
-    if(((rx_frame.id >> 8) & 0x7f) != (uint32_t) moteus_controller_.options().id) return; /* This should not happen! (map frame to wrapper first) */
+    if(((rx_frame.id >> 8) & 0x7f) != (uint32_t) moteus_controller_->options().id) return; /* This should not happen! (map frame to wrapper first) */
 
     mjbots::moteus::Query::Result result = mjbots::moteus::Query::Parse(rx_frame.data, rx_frame.size);
     state.position_ = result.position * rotation_to_radians_;
@@ -44,7 +60,7 @@ void MoteusWrapper::rx_frame_to_state(const CanFrame& rx_frame, ControllerState&
 void MoteusWrapper::init_to_tx_frame(CanFrame& tx_frame) 
 {
     /* create CANFD frame*/
-    mjbots::moteus::CanFdFrame can_fd_frame = moteus_controller_.MakeStop();
+    mjbots::moteus::CanFdFrame can_fd_frame = moteus_controller_->MakeStop();
 
     /* Copy data from CANFD frame to CAN frame*/
     tx_frame.id = can_fd_frame.arbitration_id;
@@ -65,7 +81,7 @@ void MoteusWrapper::start_pos_to_tx_frame(CanFrame& tx_frame, const ControllerCo
     start_command.velocity_limit = startup_coefficient_ * position_command_.velocity_limit * radians_to_rotation_;
 
     /* Create CANFD frame*/
-    mjbots::moteus::CanFdFrame can_fd_frame = moteus_controller_.MakePosition(start_command);
+    mjbots::moteus::CanFdFrame can_fd_frame = moteus_controller_->MakePosition(start_command);
     
     /* Copy data from CANFD frame to CAN frame */
     tx_frame.id = can_fd_frame.arbitration_id;
